6e78048f5254d3329d57410adbe59c7ef0c61f6b
lcrowell <lcrowell@hopeschool.org>
2013-06-25 11:34:48 -0500

    added ruby on rails app, not done yet, organized html files, added Submit page, have yet to fix links

---
+++ RUBY/collabsite/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collabsite::Application.config.secret_token = 'c152f911fc472f5ef74cd71849b1ca9b966c94274acb0869f83cc9550991499156b2342fb10c28d9af1e95c6f4ef13d66a4dfa2df1a109d74fda18023c6c74b0'
---
+++ RUBY/collabsite/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Collabsite::Application.config.session_store :cookie_store, key: '_collabsite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Collabsite::Application.config.session_store :active_record_store
